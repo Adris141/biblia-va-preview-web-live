ec5c8a7d9457d6a91feaf4644087e478cde81754
Adris141 <Adris141@users.noreply.github.com>
2026-04-17 06:18:04 +0000

    deploy: Adris141/biblia-va-preview@d3278403a03832d9b9af9bab5a8715c740b0909a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"4f3679cadc3804a16abfe00f51146f4291f9384c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"86ee39d2595de3a2a2cee84372bf925d774d3a6f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2961547181" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2907483135" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
